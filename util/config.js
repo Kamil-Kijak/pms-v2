3bdc0cee51a08201de6f169e9c41f75373ba2c79
Kamil Kijak <kamil.kijak@pixlesofte.com>
2025-12-12 22:39:48 +0100

    created file controller

--- util/config.js
+++ util/config.js
@@ -9,5 +9,7 @@ module.exports = {
     cookieSecurity:Number(process.env.COOKIE_SECURITY),
     refreshTokenKey:process.env.REFRESH_TOKEN_KEY || "inWhm0r9gfwJ_s07KEYrX",
     accessTokenKey:process.env.ACCESS_TOKEN_KEY || "K1BjP6-xTEAS8uKoq1vNm",
-    saltOrRounds:Number(process.env.SALT_OR_ROUNDS) || 12 
+    saltOrRounds:Number(process.env.SALT_OR_ROUNDS) || 12,
+    landFilesFolder:process.env.LAND_FILES_FOLDER || "landFiles",
+    fileMaxSize:process.env.FILE_MAX_SIZE ? Number(process.env.FILE_MAX_SIZE) : (10 * 1024 * 1024)
 };
