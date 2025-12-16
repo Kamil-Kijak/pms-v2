c334ebd369c7182df31dcc814d1ce6836aee4ff6
Kamil Kijak <kamil.kijak@pixlesofte.com>
2025-12-16 17:05:19 +0100

    added validation to owners routes

--- routes/owners.js
+++ routes/owners.js
@@ -4,13 +4,36 @@ const express = require("express");
 const router = express.Router();
 
 const ownerController = require('../controllers/ownerController');
+const { query, body } = require("express-validator");
 
-router.get("/get", ownerController.getOwners);
+router.get("/get", [
+    query("nameFilter").trim(),
+    query("limit").trim().optional().
+    isInt({min:0}).withMessage("limit must be a number greater or equal 0").
+    toInt(),
+], ownerController.getOwners);
 
-router.post("/insert", ownerController.insertOwner);
+router.post("/insert",[
+    body("name").trim().toUpperCase().
+    exists({checkFalsy:true}).withMessage("name is required").
+    isLength({min:1, max:100}).withMessage("name must be less or equal 100"),
+    body("phone").trim().
+    exists({checkFalsy:true}).withMessage("phone is required").
+    isMobilePhone("pl-PL").withMessage("phone is not valid phone format")
+], ownerController.insertOwner);
 
-router.put("/update", ownerController.updateOwner);
+router.put("/update",[
+    body("idOwner").trim().exists({checkFalsy:true}).withMessage("idOwner is required"),
+    body("name").trim().toUpperCase().
+    exists({checkFalsy:true}).withMessage("name is required").
+    isLength({min:1, max:100}).withMessage("name must be less or equal 100"),
+    body("phone").trim().
+    exists({checkFalsy:true}).withMessage("phone is required").
+    isMobilePhone("pl-PL").withMessage("phone is not valid phone format")
+], ownerController.updateOwner);
 
-router.delete("/delete", ownerController.deleteOwner);
+router.delete("/delete",[
+    body("idOwner").trim().exists({checkFalsy:true}).withMessage("idOwner is required"),
+], ownerController.deleteOwner);
 
 module.exports = router;
